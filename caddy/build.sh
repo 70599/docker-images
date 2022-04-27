3aff2f42fdfed3ba6499bf6feb2ba45f4456b3e5
root <root@R4S.lan>
2022-04-27 19:02:03 +0800

    update workflow

--- caddy/build.sh
+++ caddy/build.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 repo="lxhao61/integrated-examples"
+version=""
 
 cd /tmp
 
-url=$(curl -sL https://api.github.com/repos/$repo/releases | jq -r '.[0].assets[].browser_download_url' | grep 'linux' | grep -E 'x86_64|amd64' | head -n 1)
+if [ -n $version ]; then
+	url="https://github.com/lxhao61/integrated-examples/releases/download/${version}/caddy_linux_amd64.tar.gz"
+else
+	url=$(curl -sL https://api.github.com/repos/$repo/releases | jq -r '.[0].assets[].browser_download_url' | grep 'linux' | grep -E 'x86_64|amd64' | head -n 1)
+fi
+
 [ -n $url ] && wget -c $url -O - | tar -xz
 
 if [[ $? == 0 ]]; then
