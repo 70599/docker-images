c1625c88bf192c973dbc92439c81e157715eccfb
70599 <5929170+70599@users.noreply.github.com>
2022-07-20 10:44:51 +0800

    Update build.sh

--- caddy/build.sh
+++ caddy/build.sh
@@ -6,7 +6,7 @@ version=""
 cd /tmp
 
 if [ -n $version ]; then
-	url="https://github.com/lxhao61/integrated-examples/releases/download/${version}/caddy_linux_amd64.tar.gz"
+	url="https://github.com/lxhao61/integrated-examples/releases/download/${version}/caddy-linux-amd64.tar.gz"
 else
 	url=$(curl -sL https://api.github.com/repos/$repo/releases | jq -r '.[0].assets[].browser_download_url' | grep 'linux' | grep -E 'x86_64|amd64' | head -n 1)
 fi
